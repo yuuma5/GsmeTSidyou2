b5fa3836971092f6a0b875d1d7e68784050f241c
KBC11606024\kbc11606024 <yuma-k-16d@stu.kbc.ac.jp>
2018-05-01 10:47:15 +0900

    かり

---
+++ Gametaisyou/Assets/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+    void Start()
+    {
+    }
+    public void LButtonDown()
+    {
+        transform.Translate(-3, 0, 0);
+    }
+    public void RButtonDown()
+    {
+        transform.Translate(3, 0, 0);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //左矢印が押されたとき
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.Translate(-3, 0, 0);//左に「３」動かす
+        }
+        //右矢印が押されたとき
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.Translate(3, 0, 0);//右に「３」動かす
+        }
+
+    }
+
+}
+
