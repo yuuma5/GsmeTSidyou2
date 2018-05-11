ffa4b07aec4ee39f2f2ac67c64e6ad49bb023881
KBC11606024\kbc11606024 <yuma-k-16d@stu.kbc.ac.jp>
2018-05-11 13:15:58 +0900

    o-wari

---
+++ Gametaisyou/Assets/Gamemain/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    GameObject player;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.player = GameObject.Find("hasiru&zyannpu_15");
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 playerPos = this.player.transform.position;
+        transform.position = new Vector3(
+            playerPos.x+7, playerPos.y, transform.position.z);
+
+    }
+}
--- Gametaisyou/Assets/Gamemain/Player.cs
+++ Gametaisyou/Assets/Gamemain/Player.cs
@@ -5,18 +5,35 @@ using UnityEngine.SceneManagement;//シーンマネジメントを有効にす�
 
 public class Player : MonoBehaviour
 {
+    Rigidbody2D rigid2D;
+    bool utusiflg;
+    bool syori;
+    bool owari;
+
     GameObject target;
     public float speed, ratio;
 
     void Start()
     {
-        OnStart(GameObject.Find("cloud prep").gameObject);
+        this.rigid2D = GetComponent<Rigidbody2D>();
+       // OnStart(GameObject.Find("koko").gameObject);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown("a") && this.rigid2D.velocity.y == 0)
+        {
+            Debug.Log("koiyo");
+            Debug.Log("A-String");
+            OnStart(GameObject.Find("koko").gameObject);
+        }
+        
     }
 
-    public void OnStart(GameObject target)
+    void OnStart(GameObject target)
     {
         this.target = target;
         StartCoroutine(Throw());
+        //cnt++;
     }
 
     IEnumerator Throw()
@@ -45,7 +62,7 @@ public class Player : MonoBehaviour
         Vector3 P1 = new Vector3(P1x, P1y, 0);
 
         Vector3 look = P1;
-        transform.rotation = Quaternion.FromToRotation(Vector3.up, look);
+        //transform.rotation = Quaternion.FromToRotation(Vector3.up, look);
         float slerp_start_point = ratio * 0.5f;
 
         while (t <= 1 && target)
@@ -57,15 +74,16 @@ public class Player : MonoBehaviour
             if (t > slerp_start_point)
             {
                 look = target.transform.position - transform.position;
-                Quaternion to = Quaternion.FromToRotation(Vector3.up, look);
-                transform.rotation = Quaternion.Slerp(transform.rotation, to, speed * 0.5f * Time.deltaTime);
+                //Quaternion to = Quaternion.FromToRotation(Vector3.up, look);
+                //transform.rotation = Quaternion.Slerp(transform.rotation, to, speed * 0.5f * Time.deltaTime);
             }
 
             t += speed / distance * Time.deltaTime;
             yield return null;
         }
-
+        //cnt++;
         //Destroy (this.gameObject);
+
     }
 
 
