a1a85a89dbed52fea87714a07c3564cadf33e0e4
panda\GuestUser <yuuto-f-17e@stu.kbc.ac.jp>
2018-05-15 13:11:32 +0900

    太陽が昇るタイマー(仕様変更は運子）

---
+++ Gametaisyou/Assets/GameOver/SumTimer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SumTimer : MonoBehaviour {
+    float Timer;
+	// Use this for initialization
+	void Start () {
+        Timer = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Timer += Time.deltaTime;                                //秒ごとの座標を指定
+        transform.rotation = Quaternion.Euler(0, 0, -Timer);    //画像の角度を指定した座標に変更
+    }
+}
