a9728d1a5e2fbd951081570c13dc6fd6657fa43d
panda\GuestUser <yuuto-f-17e@stu.kbc.ac.jp>
2018-05-18 13:11:53 +0900

    黒電話追加

---
+++ Gametaisyou/Assets/GameOver/Kurodenwa.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kurodenwa : MonoBehaviour {
+    public float CheckTime;
+    public float UPTime;
+    public int CheckFlg;
+	// Use this for initialization
+	void Start () {
+        CheckTime = 1;
+        UPTime = 0;
+        CheckFlg = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.D)&& CheckFlg == 0)
+        {
+            CheckFlg = 1;
+        }
+        if(CheckFlg == 1)
+        {
+            UPTime += Time.deltaTime;
+            if (UPTime >= CheckTime)
+            {
+
+                GameObject.Find("Face").GetComponent<Face>().FaceGauge += 10;
+                UPTime = 0.0f;
+            }
+        }
+
+	}
+}
---
+++ Gametaisyou/Assets/GameOver/Orgel.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orgel : MonoBehaviour {
+    public float OrgelTime;
+    public int Orgel6Time;
+    public int OrgelOutTime;
+    public int OrgelFlg;
+    // Use this for initialization
+    void Start () {
+        OrgelTime = 0;
+        OrgelFlg = 0;
+        Orgel6Time = 6;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.S) && OrgelFlg == 0)
+        {
+            OrgelFlg = 1;
+
+        }
+        if (OrgelFlg == 1)
+        {
+            OrgelTime += Time.deltaTime;
+            if (OrgelTime >= Orgel6Time)
+            {
+               GameObject.Find("Face").GetComponent<Face>().FaceGauge -= 2;
+                Debug.Log("入った！");
+                OrgelOutTime += 6;
+                OrgelTime = 0.0f;
+            }
+            if (OrgelOutTime >= 30)
+            {
+                OrgelOutTime = 0;
+                OrgelFlg = 0;
+            }
+
+        }
+
+    }
+}
