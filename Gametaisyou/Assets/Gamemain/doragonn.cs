41f0e76164de9c97eed7d333bd97a2b6f0c8fc01
KBC11606024\kbc11606024 <yuma-k-16d@stu.kbc.ac.jp>
2018-06-08 10:54:27 +0900

    フォント

--- Gametaisyou/Assets/Gamemain/doragonn.cs
+++ Gametaisyou/Assets/Gamemain/doragonn.cs
@@ -2,18 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class doragonn : MonoBehaviour {
+public class doragonn : MonoBehaviour
+{
     Rigidbody2D rb;
-	// Use this for initialization
-	void Start () {
+    public bool korosudeflg;
+    public float doragonnTime;         //オルゴールの経過時間を加算する変数
+    public int doragonnLimitTime;      //６秒経ったか確認するための変数
+    public int doragonnOutTime;        //オルゴールのタイムがタイムリミットになったか確認する処理
+    public int doragonnFlg;            //オルゴールの処理がもうすでに開始されているか確認するフラグ
+    // Use this for initialization
+    // Use this for initialization
+    void Start()
+    {
         transform.Rotate(new Vector3(0.0f, 0.0f, 0.0f));
         rb = GetComponent<Rigidbody2D>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        doragonnTime = 0;
+        doragonnFlg = 1;
+        doragonnLimitTime = 180;
+        korosudeflg = false;
 
-        var v = new Vector2(0.6f, rb.velocity.x);
-        transform.Translate(v);
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (doragonnFlg == 1)
+        {
+            doragonnTime += Time.deltaTime;        //時間加算
+            if (doragonnTime >= doragonnLimitTime)
+            {
+                //GameObject.Find("Face").GetComponent<Face>().FaceGauge -= 2;     //顔ゲージ減算
+                Debug.Log("入った！");
+                doragonnOutTime += 180;
+                doragonnTime = 0.0f;
+                korosudeflg = true;
+            }
+            if (korosudeflg == true)
+            {
+                var v = new Vector2(0.6f, rb.velocity.x);
+                transform.Translate(v);
+            }
+        }
+    }
 }
